05f8ae236a8b251b42402cf65047ee081f6479f7
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2021-03-02 11:41:30 -0500

    fixed scroll to top button

--- src/components/ScrollTop/style.js
+++ src/components/ScrollTop/style.js
@@ -2,17 +2,27 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
+    height: 60,
+    width: 60,
     position: "fixed",
     bottom: 15,
     right: 20,
     padding: theme.spacing(2),
     margin: theme.spacing(2),
-    textAlign: "center",
     backgroundColor: theme.palette.primary.accent,
     color: "white",
     zIndex: 10,
     cursor: "pointer",
+    borderRadius: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
   },
+  icon: {
+    height: 30,
+    width: 30,
+    borderRadius: "100%"
+  }
 }));
 
 export default useStyles;
