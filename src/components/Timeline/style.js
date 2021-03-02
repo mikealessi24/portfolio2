5aa92ab7de7799c0de83da0801d66c8592eebf4a
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2021-03-02 12:07:34 -0500

    added small breakpoints to timeline

--- src/components/Timeline/style.js
+++ src/components/Timeline/style.js
@@ -5,14 +5,23 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1),
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.font,
+    [theme.breakpoints.down("sm")]:{
+      display: "none"
+    }
   },
   timelineContainer: {
     backgroundColor: theme.palette.primary.background,
     padding: theme.spacing(4),
+    [theme.breakpoints.down("sm")]:{
+      padding: 0,
+    }
   },
   timeline: {
     marginTop: theme.spacing(4),
     marginBottom: theme.spacing(4),
+    [theme.breakpoints.down("sm")]:{
+      margin: 0
+    }
   },
   lineConnector: {
     height: 100,
@@ -22,6 +31,11 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(1.5),
     backgroundColor: theme.palette.secondary.main,
     color: theme.palette.secondary.font,
+    [theme.breakpoints.down("sm")]:{
+      width: 95,
+      height: 30,
+      fontSize: 9
+    }
   },
   avatar: {
     margin: theme.spacing(1),
